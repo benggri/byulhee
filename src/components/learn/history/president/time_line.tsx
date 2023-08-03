cf77e7fd5f9aa9bbc227e07c709670ee194829ca
benggri <59782360+benggri@users.noreply.github.com>
2023-08-03 15:55:55 +0900

    Create time_line.tsx

---
+++ src/components/learn/history/president/time_line.tsx
@@ -0,0 +1,50 @@
+interface Tag {
+  type: string;
+  value : string;
+}
+
+export default function TimeLine(
+  {
+    index,
+    name,
+    terms,
+    tags,
+  }:{
+    index: number;
+    name: string;
+    terms: Array<String> | String[];
+    tags: Array<Tag> | Tag[] | undefined;
+  }
+) {
+  const getLabelColor = (type:string) => {
+    switch(type) {
+      case 'info' : return 'bg-blue-100';
+      case 'warn' : return 'bg-yellow-100';
+      case 'error': return 'bg-red-100';
+      case 'debug': return 'bg-gray-100';
+      default: return '';
+    }
+  }
+
+  return (
+    <li className="mb-10 ml-6">
+      <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900"></span>
+      <div className="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600">
+        <div className="items-center justify-between mb-3 sm:flex">
+          <time className="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">{terms[0]} ~ {terms[1] || ''}</time>
+          <div className="text-sm font-semibold text-gray-500 lex dark:text-gray-300">{index}대 대통령  <a href="#" className="font-semibold text-gray-900 dark:text-white hover:underline">{name}</a></div>
+        </div>
+        {
+          tags?.map((tag) => {
+            return (
+              <div className="p-3 text-xs italic font-normal text-gray-500 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-600 dark:border-gray-500 dark:text-gray-300">
+                <span className={getLabelColor(tag.type)+" text-yellow-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300"}>{tag.type}</span>
+                {tag.value}
+              </div>
+            );
+          })
+        }
+      </div>
+    </li>
+  );
+}
