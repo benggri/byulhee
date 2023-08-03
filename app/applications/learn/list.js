7e2e3260725d925f4e49d373d1d4005b1dbad3b6
benggri <59782360+benggri@users.noreply.github.com>
2023-08-03 15:50:53 +0900

    Create list.js

---
+++ app/applications/learn/list.js
@@ -0,0 +1,18 @@
+export default function handler(req, res) {
+  res.status(200)
+      .json([
+        {name: '', platform: ['',''], url: ''},
+        {name: '', platform: ['',''], url: ''},
+        {name: '', platform: ['',''], url: ''},
+        {name: '', platform: ['',''], url: ''},
+        {name: '', platform: ['',''], url: ''},
+        {name: '', platform: ['',''], url: ''},
+        {name: '', platform: ['',''], url: ''},
+        {name: '', platform: ['',''], url: ''},
+        {name: '', platform: ['',''], url: ''},
+        {name: '', platform: ['',''], url: ''},
+        {name: '', platform: ['',''], url: ''},
+        {name: '', platform: ['',''], url: ''},
+        {name: '', platform: ['',''], url: ''},
+      ]);
+}
