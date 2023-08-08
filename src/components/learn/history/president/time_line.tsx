68cd43bf713ff63a303deee8a84b110994b2be80
benggri <benggri@gmail.com>
2023-08-08 13:20:33 +0900

    update time_line

--- src/components/learn/history/president/time_line.tsx
+++ src/components/learn/history/president/time_line.tsx
@@ -31,8 +31,12 @@ export default function TimeLine(
       <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900"></span>
       <div className="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600">
         <div className="items-center justify-between mb-3 sm:flex">
-          <time className="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0 whitespace-pre-wrap">{terms.map((term) => {return ` ${term[0]} ~ ${term[1] || ''} \n`})}</time>
           <div className="text-sm font-semibold text-gray-500 lex dark:text-gray-300 whitespace-pre-wrap">{index.join(', \n')}대 대통령  <a href="#" className="font-semibold text-gray-900 dark:text-white hover:underline">{name}</a></div>
+          <time className="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">
+          {terms.map((term) => {
+            return <p className="text-xs text-rigth font-normal text-gray-400 sm:pl-2 sm:mb-0">{term[0]} ~ {term[1] || ''}</p>
+          })}
+          </time>
         </div>
         {
           tags?.map((tag, _index) => {
