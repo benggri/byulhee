eec032117cae68e1ceb814423e6a492173bfa061
changheekim <benggri@gmail.com>
2023-08-10 09:56:08 +0900

    update timeling

--- src/components/learn/history/president/PresidentTimeLine.tsx
+++ src/components/learn/history/president/PresidentTimeLine.tsx
@@ -37,24 +37,30 @@ export default function PresidentTimeLine(
     <li className="mb-10 ml-6">
       <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900"></span>
       <div className="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600">
-        <div className="grid grid-cols-3 gap-2 mb-2">
+        <div className="grid sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-2 mb-2">
           {
             image ? <Image src={image} alt={name} width={100} height={100} /> : <div></div>
           }
-          <div className="text-sm font-semibold text-gray-500 dark:text-gray-300">
-            <a href="#" className="font-semibold text-gray-900 dark:text-white hover:underline">{name}</a>
-            대통령
+          <div className="text-sm font-semibold text-gray-500 dark:text-gray-300 text-end">
+            <span className="text-end font-semibold text-gray-900 dark:text-white hover:underline">{name}</span>대통령
           </div>
-          <div className="text-sm font-semibold text-gray-500 dark:text-gray-300">
-          {
-            labels.map((label, index) => {
-              return <p key={'time_line_p_'+index}>{label.index}대 ({label.terms[0]} ~ {label.terms[1] || ''})</p>;
-            })
-          }
+          <div className='col-span-2 text-sm font-semibold text-gray-500 dark:text-gray-300'>
+              {
+                labels.map((label, index) => {
+                  return <p className='text-end' key={'time_line_p_'+index}>{label.index}대 ({label.terms[0]} ~ {label.terms[1] || ''})</p>;
+                })
+              }
           </div>
+          {/*<div className="text-sm font-semibold text-gray-500 dark:text-gray-300">*/}
+          {/*{*/}
+          {/*  labels.map((label, index) => {*/}
+          {/*    return <p key={'time_line_p_'+index}>{label.index}대 ({label.terms[0]} ~ {label.terms[1] || ''})</p>;*/}
+          {/*  })*/}
+          {/*}*/}
+          {/*</div>*/}
         </div>
         {
-          tags?.map((tag, _index) => {
+          tags && tags?.map((tag, _index) => {
             return (
               <div key={_index} className="p-3 text-xs italic font-normal text-gray-500 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-600 dark:border-gray-500 dark:text-gray-300">
                 <span className={getLabelColor(tag.type)+" text-yellow-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300"}>{tag.type}</span>
