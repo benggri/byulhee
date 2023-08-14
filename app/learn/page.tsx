7361a50e47e325dd3ea01cbc064a213476c96ca9
benggri <benggri@gmail.com>
2023-08-14 11:32:30 +0900

    update layout

---
+++ app/learn/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export default function Learn() {
+
+  return (
+    <section className="bg-white dark:bg-gray-900">
+      <div className="py-4 px-4 mx-auto max-w-screen-xl text-center lg:py-4">
+        <p className="text-5xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-5xl dark:text-white">Learn</p>
+      </div>
+    </section>
+  );
+}
